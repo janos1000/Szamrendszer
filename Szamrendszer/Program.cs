f94a09df8339371e643b9d3dac5ce14e9ff2b828
janos1000 <birinyij@kkszki.hu>
2024-10-04 13:18:44 +0200

    The szamrendszer projekt is working and ready to use.

---
+++ Szamrendszer/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Szamrendszer
+{
+    internal class Program
+    {
+        static void Main(string[] args) { 
+        
+                Console.Write("Kérem, adjon meg egy tizedes számot: ");
+                int decimalNumber = int.Parse(Console.ReadLine());
+                string binaryNumber = ToBinary(decimalNumber);
+                Console.WriteLine($"A szám kettes számrendszerbeli megfelelője: {binaryNumber}");
+            }
+
+            static string ToBinary(int n)
+            {
+                if (n == 0)
+                    return "0";
+
+                string binary = "";
+                while (n > 0)
+                {
+                    binary = (n % 2) + binary;
+                    n /= 2;
+                }
+                return binary;
+            }
+        }
+    }
