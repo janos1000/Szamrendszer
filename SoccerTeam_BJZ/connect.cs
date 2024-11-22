6ec01bed1d307d192230ce6082e4d0305d7bb9c3
janos1000 <birinyij@kkszki.hu>
2024-11-22 12:52:18 +0100

    Orai munka vége.

---
+++ SoccerTeam_BJZ/connect.cs
@@ -0,0 +1,28 @@
+﻿using MySql.Data.MySqlClient;
+
+
+namespace SoccerTeam
+{
+    public class connect
+    {
+        public MySqlConnection Connection;
+        public string Host;
+        public string Database;
+        public string User;
+        public string Password;
+        public string ConnectionString;
+
+        public connect()
+        {
+            Host = "localhost";
+            Database = "team";
+            User = "root";
+            Password = "";
+
+            ConnectionString = "SERVER=" + Host + ";DATABASE=" + Database + ";UID=" +
+                User + ";PASSWORD=" + Password + ";SslMode=None";
+
+            Connection = new MySqlConnection(ConnectionString);
+        }
+    }
+}
